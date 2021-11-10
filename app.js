8ce93df5e2ab5631e0ea14a6c45d5097a877c498
CaptAlpha <arhitbose.tagore2020@vitstudent.ac.in>
2021-11-10 12:21:35 +0530

    Added stylesheet

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const Application = PIXI.Application;
 
-const app = new Application();
+const app = new Application({
+    width: 800,
+    height: 800,
+    transparent: false,
+    antialias: false
+}
+);
+app.renderer.backgroundColor = 0xA0E7E5;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+app.renderer.view.style.position = 'absolute';
+
+
 document.body.appendChild(app.view);
