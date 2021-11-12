22d2b8f6be0c8e4ed9a39823ebbfe4822fb313d3
CaptAlpha <arhitbose.tagore2020@vitstudent.ac.in>
2021-11-12 17:10:51 +0530

    Added PIXI Extra Graphics

--- app.js
+++ app.js
@@ -13,3 +13,35 @@ app.renderer.view.style.position = 'absolute';
 
 
 document.body.appendChild(app.view);
+
+const Graphics = PIXI.Graphics;
+
+const rectangle = new Graphics();
+rectangle.beginFill(0xB4F8C8)
+.lineStyle(4, 0xF652A0, 1)
+.drawRect(200, 200, 100, 120)
+.endFill();
+
+app.stage.addChild(rectangle);
+
+const circle = new Graphics();
+circle.beginFill(0xB4F8C8)
+.lineStyle(4, 0xF652A0, 1)
+.drawCircle(200, 200, 100)
+.endFill();
+
+app.stage.addChild(circle);
+
+const line = new Graphics();
+line.lineStyle(4, 0xF652A0, 1)
+.moveTo(2000, 2000)
+.lineTo(200, 200)
+
+app.stage.addChild(line);
+
+const torus = new Graphics();
+torus.beginFill(0xFFFFFF)
+.drawTorus(100, 600, 80, 100)
+.endFill();
+
+app.stage.addChild(torus);
